d8362a7bb51a38ccb1542da45012693e355c8e0a
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2023-11-05 01:35:52 +0700

    first commit

---
+++ src/main/java/murach/business/User.java
@@ -0,0 +1,60 @@
+package murach.business;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+	private Long userId;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    @Id
+    @Column(name = "UserId")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+    @Column(name = "Email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    @Column(name = "FirstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "LastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
